277aee228a30f59a8f3ca290e7706b7876b2bf19
Joanna <jlapa.transport@gmail.com>
2023-09-05 14:57:08 +0200

    CustomFileter basic structure

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CustommFilterProps } from '@/types'
+
+function CustomFilter({title}: CustommFilterProps) {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default CustomFilter
