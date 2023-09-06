8d874e8344b52fa742f64854906465a0fd00f1f4
Joanna <jlapa.transport@gmail.com>
2023-09-06 14:51:01 +0200

    imagin studio API & generateCarImageUrl function implemented,

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import CarDetails from './CarDetails';
 import React from 'react';
 import { useState } from 'react';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import Image from 'next/image';
 
 interface CarCardProps {
@@ -33,7 +33,7 @@ const CarCard = ({ car }: CarCardProps) => {
 			</p>
 
 			<div className='relative w-full h-40 my-3 object-contain'>
-				<Image src='/hero.png' alt='Car model' fill priority className='object-contain' />
+				<Image src={generateCarImageUrl(car)} alt='Car model' fill priority className='object-contain' />
 			</div>
 
 			<div className='relative flex w-full mt-2'>
--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,7 @@ declare global {
       interface ProcessEnv {
         NEXT_PUBLIC_RAPID_API_KEY: string;
         NEXT_PUBLIC_RAPID_API_HOST: string;
+        NEXT_PUBLIC_IMAGIN_API_KEY: string;
       }
     }
   }
